69ab24a75a23b04b544a0c3985834b1aa67eac28
rahal13001 <rahal13001@mail.unpad.ac.id>
2025-08-11 21:34:22 +0900

    Add province relationship to City model

--- app/Models/City.php
+++ app/Models/City.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     protected $fillable = ['name','province_id'];
+
+    public function province()
+    {
+        return $this->belongsTo(Province::class);
+    }
 }
