d9a5e40809b8bc88ff68cb2d171ca15ca5e59b8c
rahal13001 <rahal13001@mail.unpad.ac.id>
2025-08-11 21:37:05 +0900

    Add partner relationship and fillable fields to Sipji model

--- app/Models/Sipji.php
+++ app/Models/Sipji.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sipji extends Model
 {
-    //
+    protected $fillable = ['partner_id', 'permit_number', 'issue_date', 'expiry_date'];
+
+    public function partner()
+    {
+        return $this->belongsTo(Partner::class);
+    }
 }
